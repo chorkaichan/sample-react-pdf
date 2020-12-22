751673a48c1dacc813b438f5b3df8c2c562dca5e
Kaito Kikuchi <kaito@soundonlive.com>
2020-12-22 19:05:18 +0900

    Show pdf and highlights

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,82 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+/* eslint-disable */
+import React, { useState } from 'react';
+// @ts-ignore
+import { Document, Page } from '../node_modules/react-pdf/dist/esm/entry.webpack.js';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+function highlightPattern(text: string, pattern: string) {
+  const splitText = text.split(pattern);
 
-export default IndexPage
+  if (splitText.length <= 1) {
+    return text;
+  }
+
+  const matches: any = text.match(pattern);
+
+  return splitText.reduce((arr: any[], element: any, index: number) => (matches[index] ? [
+    ...arr,
+    element,
+    <mark key={index}>
+      {matches[index]}
+    </mark>,
+  ] : [...arr, element]), []);
+}
+
+export default function Sample() {
+  const [file, setFile] = useState('');
+  const [numPages, setNumPages] = useState(null);
+  const [searchText, setSearchText] = useState('');
+
+  function onChange(event: any) {
+    setSearchText(event.target.value);
+  }
+
+  function onFileChange(event: any) {
+    setFile(event.target.files[0]);
+  }
+
+  function onDocumentLoadSuccess({ numPages: nextNumPages }: any) {
+    setNumPages(nextNumPages);
+  }
+
+  return (
+    <div className="Example">
+      <header>
+        <h1>react-pdf sample page</h1>
+      </header>
+      <div className="Example__container">
+        <div className="Example__container__load">
+          <label htmlFor="file">Load from file:</label>
+          {' '}
+          <input
+            onChange={onFileChange}
+            type="file"
+          />
+        </div>
+        <div className="Example__container__document">
+          <Document
+            file={file}
+            onLoadSuccess={onDocumentLoadSuccess}
+          >
+            {
+              Array.from(
+                new Array(numPages),
+                (_, index) => (
+                  <Page
+                    key={`page_${index + 1}`}
+                    pageNumber={index + 1}
+                    customTextRenderer={({ str }: any) => highlightPattern(str, searchText)}
+                  />
+                ),
+              )
+            }
+          </Document>
+        </div>
+        <div>
+          <label htmlFor="search">Search:</label>
+          <input type="search" id="search" value={searchText} onChange={onChange} />
+        </div>
+      </div>
+    </div>
+  );
+}
